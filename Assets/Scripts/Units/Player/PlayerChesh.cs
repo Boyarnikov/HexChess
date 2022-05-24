6ac1afa8c33066837207eac5f38377f58474f236
boyarnikov <boiarnikov.ia@phystech.edu>
2022-05-24 05:59:30 +0300

    player logic added to scene, pawns are now consistant with move rules and with directions

--- Assets/Scripts/Units/Player/PlayerChesh.cs
+++ Assets/Scripts/Units/Player/PlayerChesh.cs
@@ -7,9 +7,24 @@ public class PlayerChesh : BasePlayer
     public override List<Tile> GetAllMoves() {
         var list = new List<Tile>();
         var pos = _tile.GetCoordinates();
+        int mindist = 100;
+        var distances = new Dictionary<Vector2, int>();
+
         foreach(var dir in Directions.all) {
+            var dist = 0;
+            var inPos = pos;
+            while (GridManager.Instance.GetTile(inPos) != null) {
+                inPos += dir;
+                dist ++;
+            }
+            mindist = Mathf.Min(dist, mindist);
+            distances[dir] = dist;
+        }
+
+        foreach(var dir in Directions.all) {
+            if (distances[dir] > mindist) continue;
             var tile = GridManager.Instance.GetTile(dir+pos);
-            if (tile != null) 
+            if (tile != null && tile.Free) 
                 list.Add(tile);
         }
         return list;
