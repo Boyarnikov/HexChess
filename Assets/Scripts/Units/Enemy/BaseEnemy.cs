e10016f1ee980bf63ca71b398309ca1d620ffde2
boyarnikov <boiarnikov.ia@phystech.edu>
2022-05-25 10:06:00 +0300

    simple AI added

--- Assets/Scripts/Units/Enemy/BaseEnemy.cs
+++ Assets/Scripts/Units/Enemy/BaseEnemy.cs
@@ -26,10 +26,43 @@ public class BaseEnemy : BaseUnit
         if (_renderUpdateNeeded) {UpdateRenderer();}
     }
 
+    int EstimatCost(MoveType _moveType) {
+        switch (_moveType)
+        {
+            case MoveType.Rook:
+                return 5;
+            case MoveType.King:
+                return 10;
+            case MoveType.Fool:
+                return 2;
+            case MoveType.ChechFromCenter:
+                return 1;
+            case MoveType.Knight:
+                return 3;
+            default:
+                return 0;
+        }
+    }
+
     public virtual void Move() {
         var moves = GetAllMoves();
         if (moves == null) return;
-        var move = moves[Random.Range(0, moves.Count)];
+        Tile move;
+        var newMoves = new List<Tile>();
+        int best_move = 0;
+        foreach (var m in moves)
+        {
+            if (m._unit != null && m._unit._type == Type.player) {
+                if (best_move < EstimatCost(m._unit._moveType)) {
+                    best_move = EstimatCost(m._unit._moveType);
+                    newMoves = new List<Tile>();
+                }
+                if (best_move == EstimatCost(m._unit._moveType)) {
+                    newMoves.Add(m);
+                }
+            }
+        }
+        move = newMoves[Random.Range(0, newMoves.Count)];
         if (move == null) return;
         if (move._unit == null || move._unit != null && move._unit._type != _type) {
             if (!move.Free) {
